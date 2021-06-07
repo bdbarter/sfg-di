c8fe06eb061d072f43fd389fc851e02d6e6743b1
Brett <barterianb@gmail.com>
2021-06-06 21:09:03 -0400

    Profile example with i18nController

---
+++ src/main/java/com/example/sfgdi/services/I18nSpanishService.java
@@ -0,0 +1,16 @@
+package com.example.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by bb on 6/6/2021.
+ */
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
